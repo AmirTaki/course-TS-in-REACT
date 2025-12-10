428cf8c54f2326f0d39fde781fc0841529e0bec9
amir <amirtaki623@gmail.com>
2025-12-10 03:51:47 +0330

    install react ts & tailiwn in project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     <p
+     className='bg-yellow-400'
+     >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur officiis adipisci porro maxime, dolor doloribus quisquam modi alias iste tenetur neque maiores, fugiat unde labore similique quia molestiae aliquam eaque.</p>
+    </>
+  )
+}
+
+export default App
