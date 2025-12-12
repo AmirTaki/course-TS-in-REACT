813794c3186e0c85d33020e3675c28c1306a5a9b
amir <amirtaki623@gmail.com>
2025-12-12 19:16:28 +0330

    ComponentPropsWithoutRef part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 'use client'
-import React from 'react'
 import './App.css'
 import Button from './component/Button'
 
 function App() {
-  const icon = <i>Click me!</i>
-  const [count, setCount] = React.useState(0)
   
   return (
       <div className='flex justify-center items-center h-screen'>
         <Button 
-          setCount = {setCount}
-          count = {count}
+          type = 'submit'
+          autoFocus = {true}
         >
-          {icon}
+          ClickMe
         </Button>
       </div>  
   )
--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,31 +1,25 @@
-import type { JSX } from "react"
 
 
 interface IButtonProps {
-    children: JSX.Element,
-    count: number
-    setCount: React.Dispatch<React.SetStateAction<number>>
-}
+    children: React.ReactNode,
+    type : "submit" | 'reset' | 'button' ,
+    autoFocus : boolean
 
-type Color = "red" | "blue" | 'green'
-
-interface Colors  {
-    color: 'red' | 'silver' | 'blue'
+     
 }
 
 
-
-const Button = ({children, count, setCount}: IButtonProps) => {
+const Button = ({children, type, autoFocus}: IButtonProps) => {
 
 
     return(
         <>
             <button
-                onClick={() => {setCount((prevCount: number) => (prevCount + 1))}}
+                type = {type}
+                autoFocus = {autoFocus}
                 className="rounded  py-2 bg-blue-500 text-white px-3">
                 {children}
             </button>
-            {count}
         </>
     )
 }
