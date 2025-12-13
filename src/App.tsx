00a99730fae3581c65072d616e830456606e53c6
amir <amirtaki623@gmail.com>
2025-12-13 05:39:27 +0330

    useState part 2:

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import {  useState } from 'react'
 import './App.css'
 
-interface User<T, K> {
-  name: T, 
-  age: K
+interface User {
+  name: string, 
+  age: number
 }
 function App() {
   
-  const [conter, setConter]  = useState <Array<User<string, number>> | null>([])
+  const [conter, setConter]  = useState<number>(0)
+  const [user, setUser] = useState<User[] | null >(null)
   
-  const [user, setUser] = useState<User <number, string>> ({} as User<number, string>)
   return (
       <div className='flex justify-center items-center h-screen'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem laboriosam reprehenderit nam, perspiciatis error vitae deleniti optio, non impedit porro voluptates minima eligendi excepturi obcaecati at quae? Nemo, hic? Eligendi.
