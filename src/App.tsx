b1b52c054020a8cc71e6d04e2bd120c85743466f
amir <amirtaki623@gmail.com>
2025-12-13 05:49:04 +0330

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {  useState } from 'react'
+import {  useEffect, useState } from 'react'
 import './App.css'
 
 interface User {
@@ -8,8 +8,14 @@ interface User {
 function App() {
   
   const [conter, setConter]  = useState<number>(0)
-  const [user, setUser] = useState<User[] | null >(null)
+  const [users, setUsers] = useState<User[] | null >(null)
   
+  useEffect(() => {
+    console.log('mounting')
+    console.log(`users: ${users}`)
+
+    return () => {console.log ('unmounting')}
+  }, [users])
   return (
       <div className='flex justify-center items-center h-screen'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem laboriosam reprehenderit nam, perspiciatis error vitae deleniti optio, non impedit porro voluptates minima eligendi excepturi obcaecati at quae? Nemo, hic? Eligendi.
