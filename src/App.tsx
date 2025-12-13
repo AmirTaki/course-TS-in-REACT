3e3e291d82b754667918f6fb3b5a920908876d21
amir <amirtaki623@gmail.com>
2025-12-13 21:15:52 +0330

    use Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,15 @@
-import React, {  useCallback, useEffect, useMemo, useRef, useState,  } from 'react'
 import './App.css'
-
-interface User {
-  name: string, 
-  age: number
-}
-
-type fibFunc = (n: number)  => number
-
-const fib: fibFunc = (n) => {
-  if (n < 2){ return 1}
-  else {
-    // return fib(n - 1) + fib(n + 2)
-    return fib(n - 1)
-  }
-}
-
-const myNum: number = 30;
-
-// console.log(fib(myNum))
-
+import Child from './component/Child'
 
 function App() {
   
-  const [count, setCount]  = useState<number>(0)
-  const [users, setUsers] = useState<User[] | null >(null)
-
-  const inputRef = useRef<HTMLInputElement >(null!)
-  
-  console.log(inputRef?.current)
-  console.log(inputRef?.current?.value)
-
-  useEffect(() => {
-    console.log('mounting')
-    console.log(`users: ${users}`)
-
-    return () => {console.log ('unmounting')}
-  }, [users])
-
-
-  const addTwo = useCallback((e: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent<HTMLButtonElement>): void => {
-    setCount((prev) => prev + 1)
-    console.log(e)
-  }, [])
-
-
-  const results = useMemo<number>(() => fib(myNum), [myNum])
 
   return (
-      <div className='flex justify-center items-center h-screen flex-col'>
-        <h1>{count}</h1>
-        <button
-          className='border-2 border-red-700'
-          onClick={addTwo}
-        >
-          ADD
-        </button>
-
-        <h2 className='bg-emerald-400'>{results}</h2>
-
-        <input 
-          type = "text"
-          className='border-2 border-solid border-amber-700'
-          ref = {inputRef}
-        ></input>
-      </div>  
+      <div className=" flex flex-col justify-center items-center h-screen">
+        <Child >
+          {(num: number) => <>Current Count : {num}</>}
+        </Child>
+      </div>
   )
 }
 
