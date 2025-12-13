663bd753d254a74775bd543e22176783c0bc0a01
amir <amirtaki623@gmail.com>
2025-12-13 05:37:35 +0330

    useState  part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-'use client'
+import {  useState } from 'react'
 import './App.css'
-import Button from './component/Button'
+
+interface User<T, K> {
+  name: T, 
+  age: K
+}
 function App() {
   
-
+  const [conter, setConter]  = useState <Array<User<string, number>> | null>([])
+  
+  const [user, setUser] = useState<User <number, string>> ({} as User<number, string>)
   return (
       <div className='flex justify-center items-center h-screen'>
-        <Button  />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem laboriosam reprehenderit nam, perspiciatis error vitae deleniti optio, non impedit porro voluptates minima eligendi excepturi obcaecati at quae? Nemo, hic? Eligendi.
       </div>  
   )
 }
